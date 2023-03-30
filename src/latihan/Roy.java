4d2b029f0b8fabcfbd2f1988e54c1a43a3b4cbaa
Harwas <royharwamasatu06@gmail.com>
2023-03-30 17:38:04 +0700

    owe

--- src/latihan/Roy.java
+++ src/latihan/Roy.java
@@ -9,5 +9,10 @@ package latihan;
  * @author ASUS
  */
 public class Roy {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello this is Roy");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+    }
 }
